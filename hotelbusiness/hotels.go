f97108956c25ca1ccfd74d7c610fd51b723cb9ed
Dmitrii.Proskurin <dimapr117@gmail.com>
2025-06-05 00:37:49 +0300

    hotelbusiness

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"sort"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +17,30 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	loadChng := make(map[int]int)
+	for _, g := range guests {
+		loadChng[g.CheckInDate]++
+		loadChng[g.CheckOutDate]--
+	}
+
+	dates := make([]int, 0)
+	for k := range loadChng {
+		dates = append(dates, k)
+	}
+	sort.Ints(dates)
+
+	res := make([]Load, 0)
+	curLoad := 0
+	for _, d := range dates {
+		if loadChng[d] == 0 {
+			continue
+		}
+		curLoad += loadChng[d]
+		res = append(res, Load{
+			StartDate:  d,
+			GuestCount: curLoad,
+		})
+	}
+
+	return res
 }
