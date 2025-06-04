8672d76acbe1c1a0c4f97d9bda267431a3806440
Dmitrii.Proskurin <dimapr117@gmail.com>
2025-06-05 00:47:21 +0300

    ratelimit

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -10,6 +10,11 @@ import (
 
 // Limiter is precise rate limiter with context support.
 type Limiter struct {
+	sema      chan struct{}
+	releaseTs chan time.Time
+	interval  time.Duration
+	stopped   bool
+	abortJob  chan struct{}
 }
 
 var ErrStopped = errors.New("limiter stopped")
@@ -17,13 +22,43 @@ var ErrStopped = errors.New("limiter stopped")
 // NewLimiter returns limiter that throttles rate of successful Acquire() calls
 // to maxSize events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
-	panic("not implemented")
+	limiter := &Limiter{
+		sema:      make(chan struct{}, maxCount),
+		releaseTs: make(chan time.Time, maxCount),
+		interval:  interval,
+		stopped:   false,
+		abortJob:  make(chan struct{}, 1),
+	}
+	go limiter.releaseJob()
+	return limiter
 }
 
 func (l *Limiter) Acquire(ctx context.Context) error {
-	panic("not implemented")
+	if l.stopped {
+		return ErrStopped
+	}
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case l.sema <- struct{}{}:
+		l.releaseTs <- time.Now().Add(l.interval)
+	}
+	return nil
 }
 
 func (l *Limiter) Stop() {
-	panic("not implemented")
+	l.stopped = true
+	l.abortJob <- struct{}{}
+	close(l.releaseTs)
+}
+
+func (l *Limiter) releaseJob() {
+	for ts := range l.releaseTs {
+		select {
+		case <-time.After(time.Until(ts)):
+			<-l.sema
+		case <-l.abortJob:
+			return
+		}
+	}
 }
