ce9bc924c66a47e2b3f1e2941ec8037d198c26d7
Dmitrii.Proskurin <dimapr117@gmail.com>
2025-06-05 00:38:28 +0300

    utf8

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,31 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+const SPACE rune = ' '
+
 func CollapseSpaces(input string) string {
-	return ""
+	sb := strings.Builder{}
+	sb.Grow(len(input))
+	prevIsSpace := false
+	for len(input) > 0 {
+		r, size := utf8.DecodeRuneInString(input)
+		input = input[size:]
+		curIsSpace := unicode.IsSpace(r)
+		if prevIsSpace && curIsSpace {
+			continue
+		}
+		if curIsSpace {
+			sb.WriteRune(SPACE)
+		} else {
+			sb.WriteRune(r)
+		}
+		prevIsSpace = curIsSpace
+	}
+	return sb.String()
 }
