1818a51089f4e1416e33d8d6f36860503597f819
Dmitrii.Proskurin <dimapr117@gmail.com>
2025-06-05 00:35:15 +0300

    wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,30 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	wordCount := make(map[string]int)
+
+	for _, filePath := range os.Args[1:] {
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			panic(err)
+		}
+
+		dataS := string(data)
+		for _, word := range strings.Split(dataS, "\n") {
+			wordCount[word]++
+		}
+	}
+
+	for word, cnt := range wordCount {
+		if cnt > 1 {
+			fmt.Printf("%d\t%s\n", cnt, word)
+		}
+	}
 }
