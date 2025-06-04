d34e06b5c672625c5414423f57023088ef1d803b
Dmitrii.Proskurin <dimapr117@gmail.com>
2025-06-05 00:35:43 +0300

    fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,48 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
 
+func fetchURL(url string, results chan<- string) {
+	start := time.Now()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		results <- err.Error()
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		results <- err.Error()
+		return
+	}
+
+	dur := time.Since(start)
+	results <- fmt.Sprintf("url %s result len %d; time %v", url, len(string(body)), dur)
+}
+
+func main() {
+	results := make(chan string)
+	urlsCnt := 0
+
+	startTime := time.Now()
+	for _, url := range os.Args[1:] {
+		go fetchURL(url, results)
+		urlsCnt++
+	}
+
+	for i := 0; i < urlsCnt; i++ {
+		res := <-results
+		fmt.Println(res)
+	}
+
+	fmt.Println(time.Since(startTime))
 }
